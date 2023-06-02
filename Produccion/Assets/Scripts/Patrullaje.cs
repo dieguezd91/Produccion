92053efa490449852d059ad00d15f1138831b490
Daniel Dieguez <dieguezd91@gmail.com>
2023-06-01 22:10:44 -0300

    patrullaje

--- Produccion/Assets/Scripts/Patrullaje.cs
+++ Produccion/Assets/Scripts/Patrullaje.cs
@@ -7,11 +7,13 @@ public class Patrullaje : MonoBehaviour
     [SerializeField] private GameObject[] waypoints;
     private int currentWaypointIndex = 0;
 
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private float speed;
 
     void Update()
     {
-        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .2f)
         {
             currentWaypointIndex++;
             if (currentWaypointIndex >= waypoints.Length)
@@ -19,7 +21,5 @@ public class Patrullaje : MonoBehaviour
                 currentWaypointIndex = 0;
             }
         }
-        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
-
     }
 }
