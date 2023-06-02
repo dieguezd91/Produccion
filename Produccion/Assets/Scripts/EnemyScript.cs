1c55401876fa15a7884a1a955d4ed9b45beda037
Daniel Dieguez <dieguezd91@gmail.com>
2023-06-02 02:53:55 -0300

    update

--- Produccion/Assets/Scripts/EnemyScript.cs
+++ Produccion/Assets/Scripts/EnemyScript.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public string[] names;
-    void Update()
-    {
-        
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             BattleManager.instance.StartBattle(names);
         }
