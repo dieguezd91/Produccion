4f8dd36c27cc4c2e846323765d1407061d6e5d1b
ElTabor <105396632+ElTabor@users.noreply.github.com>
2023-06-23 19:34:17 -0300

    Instant random combat bug

--- Produccion/Assets/Scripts/Battle System/RandomBattle.cs
+++ Produccion/Assets/Scripts/Battle System/RandomBattle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,19 +6,38 @@ using UnityEngine;
 
 public class RandomBattle : MonoBehaviour
 {
+    Collider2D colider;
+    private void Start()
+    {
+        colider = GetComponent<Collider2D>();
+        BattleManager.instance.OnBattleEnd += Scape;
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
-            int i = Random.Range(1, 101);
+            int i = UnityEngine.Random.Range(1, 101);
             string enemy;
             if (i <= 5) enemy = "Mercenario";
             else enemy = "Maton";
 
             if ( i <= 10)
             {
+                Debug.Log("Combate random!");
                 BattleManager.instance.StartBattle(null, enemy, true);
             }
         }
+    }
+
+    void Scape( object sender, EventArgs e)
+    {
+        StartCoroutine(Inmunity());
+    }
+
+    IEnumerator Inmunity()
+    {
+        colider.enabled = false;
+        yield return new WaitForSeconds(3f);
+        colider.enabled = true;
     }
 }
