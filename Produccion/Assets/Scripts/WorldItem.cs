a305933f959b3dbc5ccdfcd495c4c24a065c9b52
ElTabor <105396632+ElTabor@users.noreply.github.com>
2023-07-04 18:04:40 -0300

    WorldItems

---
+++ Produccion/Assets/Scripts/WorldItem.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorldItem : MonoBehaviour
+{
+    [SerializeField] ItemsManager item;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("item recogido");
+            gameObject.SetActive(false);
+            Inventory.instance.AddItems(item);
+        }
+    }
+}
